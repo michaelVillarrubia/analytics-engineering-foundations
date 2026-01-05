60f37a9e603fbc2b5232911a4af643c5d4917f4f
Michael Villarrubia <villarrubia.m01@mymail.sxu.edu>
2026-01-05 16:53:14 -0600

    Reinitialize repository

---
+++ src/main.py
@@ -0,0 +1,45 @@
+import csv
+from collections import Counter
+
+def count_column_values(file_path, column_name):
+    counts = Counter()
+
+    with open(file_path, newline="") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            value = row[column_name]
+            counts[value] += int(row["amount"])
+
+    return counts
+
+def create_csv_file(filename, content):
+    with open("data/raw/" + filename + ".csv", 'w') as file:
+        file.write(content)
+    print(f"File '{filename}' created and written successfully.")
+
+def gettingBackIntoIt():
+    #splice 
+    word = "splice"
+    print(word[0:2])
+
+    #list practice
+    languages = ['sql', 'c#', 'java', 'javascript', 'c']
+    print(languages)
+    #append
+    languages.append('python')
+    print(languages)
+    #slice
+    languages[1:3] = []
+    print(languages)
+    languages.append('c#')
+    languages.append('java')
+    print(languages)
+
+
+def main():
+    gettingBackIntoIt()
+    results = count_column_values("data/raw/sample.csv", "category")
+    print(results)
+
+if __name__ == "__main__":
+    main()
